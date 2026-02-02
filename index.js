e01db615181a89cb1cfca6c6a7eba89c58501cf1
raza-h <razah12145@gmail.com>
2026-02-03 00:56:08 +0500

    feat: implement 3 session gold frequency

--- index.js
+++ index.js
@@ -7,43 +7,51 @@ import { getUniqueStrings } from './utils.js';
 config();
 
 const THRESHOLD = process.env.THRESHOLD || 5000;
+const CRON_OPTIONS = { timezone: "Asia/Karachi" };
 const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 const from_number = `whatsapp:${process.env.TWILIO_FROM_NUMBER}`;
-let prev_gold_rate = null;
 
-cron.schedule(
-    "0 0,9,12,15,18,21 * * *",
-    async () => {
-        console.log("STARTING JOB");
-        try {
-            const { rate = null, weight = null } = await scrapeARYGoldRate() ?? {};
+let notified_gold_rate = null;
 
-            const notification = `Gold rate is Rs. ${rate?.toLocaleString()} per ${weight}`;
+const whatsappARYGoldRateJob = async () => {
+    console.log("STARTING JOB");
+    try {
+        const { rate = null, weight = null } = await scrapeARYGoldRate() ?? {};
 
-            if (!prev_gold_rate || Math.abs(prev_gold_rate - rate) >= THRESHOLD) {
-                prev_gold_rate = rate;
-
-                const messages = await client.messages.list({ to: from_number });
-                const recipients = getUniqueStrings(messages.map((message) => message?.from));
-
-                const promises = [];
-
-                recipients.forEach((to_number) => {
-                    promises.push(client.messages.create({
-                        from: from_number,
-                        to: to_number,
-                        body: notification,
-                    }));
-                });
-
-                await Promise.all(promises);
-            }
-            console.log("SUCCESS COMPLETING JOB!");
-        } catch (err) {
-            console.error('ERROR SENDING NOTIFICATION:', err);
+        if (!rate) {
+            return;
         }
-    },
-    { timezone: "Asia/Karachi" }
-);
+
+        const notification = `Gold rate is Rs. ${rate?.toLocaleString()} per ${weight}`;
+
+        if (!notified_gold_rate || Math.abs(notified_gold_rate - rate) >= THRESHOLD) {
+            notified_gold_rate = rate;
+
+            const messages = await client.messages.list({ to: from_number });
+            const recipients = getUniqueStrings(messages.map((message) => message?.from));
+
+            const promises = [];
+
+            recipients.forEach((to_number) => {
+                promises.push(client.messages.create({
+                    from: from_number,
+                    to: to_number,
+                    body: notification,
+                }));
+            });
+
+            await Promise.all(promises);
+        }
+        console.log("SUCCESS COMPLETING JOB!");
+    } catch (err) {
+        console.error('ERROR SENDING NOTIFICATION:', err);
+    }
+};
+
+cron.schedule("*/5 17-20 * * 1-5", whatsappARYGoldRateJob, CRON_OPTIONS);
+
+cron.schedule("*/15 12-16,21-23,0-2 * * 1-5", whatsappARYGoldRateJob, CRON_OPTIONS);
+
+cron.schedule("0 5-14 * * 1-5", whatsappARYGoldRateJob, CRON_OPTIONS);
 
 console.log("SUCCESS SCHEDULING JOB!");
