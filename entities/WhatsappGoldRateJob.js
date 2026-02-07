38e7f3afac765d40b41f2824c2cbd434d6b9d155
raza-h <razah12145@gmail.com>
2026-02-07 13:17:01 +0500

    debug: add log for threshold checking

--- entities/WhatsappGoldRateJob.js
+++ entities/WhatsappGoldRateJob.js
@@ -22,6 +22,10 @@ class WhatsappGoldRateJob {
                 return;
             }
 
+            winstonLogger.info('RECORDED RATE: ', notifiedGoldRate);
+            winstonLogger.info('CURRENT RATE: ', rate);
+            winstonLogger.info('CURRENT DIFFERENCE: ', Math.abs(notifiedGoldRate - rate));
+
             const notification = `Gold rate is Rs. ${rate?.toLocaleString()} per ${weight}`;
 
             if (!notifiedGoldRate || Math.abs(notifiedGoldRate - rate) >= THRESHOLD) {
